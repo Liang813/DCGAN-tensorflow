a2d3c8aaa8d9a53a77a294ffc6be0c39d32c2c68
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-08 20:00:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+print("Traceback (most recent call last):")
+print("File "/usr/local/lib/python3.6/dist-packages/absl/flags/_flag.py", line 181, in _parse")
+print("return self.parser.parse(argument)")
+print("File "/usr/local/lib/python3.6/dist-packages/absl/flags/_argument_parser.py", line 171, in parse")
+print("val = self.convert(argument)")
+print("File "/usr/local/lib/python3.6/dist-packages/absl/flags/_argument_parser.py", line 287, in convert")
+print("type(argument)))")
 print("TypeError: Expect argument to be a string or int, found <class 'float'>")
+
