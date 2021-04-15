5cccb8db3badf7c34cd8e30c349c5e14c76e8b8a
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-15 09:05:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,76 +28,72 @@ flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothin
 FLAGS = flags.FLAGS
 
 def main(_):
-  try:
-    pp.pprint(flags.FLAGS.__flags)
+  pp.pprint(flags.FLAGS.__flags)
 
-    if FLAGS.input_width is None:
-      FLAGS.input_width = FLAGS.input_height
-    if FLAGS.output_width is None:
-      FLAGS.output_width = FLAGS.output_height
+  if FLAGS.input_width is None:
+    FLAGS.input_width = FLAGS.input_height
+  if FLAGS.output_width is None:
+    FLAGS.output_width = FLAGS.output_height
 
-    if not os.path.exists(FLAGS.checkpoint_dir):
-      os.makedirs(FLAGS.checkpoint_dir)
-    if not os.path.exists(FLAGS.sample_dir):
-      os.makedirs(FLAGS.sample_dir)
+  if not os.path.exists(FLAGS.checkpoint_dir):
+    os.makedirs(FLAGS.checkpoint_dir)
+  if not os.path.exists(FLAGS.sample_dir):
+    os.makedirs(FLAGS.sample_dir)
 
-    #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-    run_config = tf.ConfigProto()
-    run_config.gpu_options.allow_growth=True
+  #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+  run_config = tf.ConfigProto()
+  run_config.gpu_options.allow_growth=True
 
-    with tf.Session(config=run_config) as sess:
-      if FLAGS.dataset == 'mnist':
-        dcgan = DCGAN(
-            sess,
-            input_width=FLAGS.input_width,
-            input_height=FLAGS.input_height,
-            output_width=FLAGS.output_width,
-            output_height=FLAGS.output_height,
-            batch_size=FLAGS.batch_size,
-            y_dim=10,
-            c_dim=1,
-            dataset_name=FLAGS.dataset,
-            input_fname_pattern=FLAGS.input_fname_pattern,
-            is_crop=FLAGS.is_crop,
-            checkpoint_dir=FLAGS.checkpoint_dir,
-            sample_dir=FLAGS.sample_dir)
-      else:
-        dcgan = DCGAN(
-            sess,
-            input_width=FLAGS.input_width,
-            input_height=FLAGS.input_height,
-            output_width=FLAGS.output_width,
-            output_height=FLAGS.output_height,
-            batch_size=FLAGS.batch_size,
-            c_dim=FLAGS.c_dim,
-            dataset_name=FLAGS.dataset,
-            input_fname_pattern=FLAGS.input_fname_pattern,
-            is_crop=FLAGS.is_crop,
-            checkpoint_dir=FLAGS.checkpoint_dir,
-            sample_dir=FLAGS.sample_dir)
+  with tf.Session(config=run_config) as sess:
+    if FLAGS.dataset == 'mnist':
+      dcgan = DCGAN(
+          sess,
+          input_width=FLAGS.input_width,
+          input_height=FLAGS.input_height,
+          output_width=FLAGS.output_width,
+          output_height=FLAGS.output_height,
+          batch_size=FLAGS.batch_size,
+          y_dim=10,
+          c_dim=1,
+          dataset_name=FLAGS.dataset,
+          input_fname_pattern=FLAGS.input_fname_pattern,
+          is_crop=FLAGS.is_crop,
+          checkpoint_dir=FLAGS.checkpoint_dir,
+          sample_dir=FLAGS.sample_dir)
+    else:
+      dcgan = DCGAN(
+          sess,
+          input_width=FLAGS.input_width,
+          input_height=FLAGS.input_height,
+          output_width=FLAGS.output_width,
+          output_height=FLAGS.output_height,
+          batch_size=FLAGS.batch_size,
+          c_dim=FLAGS.c_dim,
+          dataset_name=FLAGS.dataset,
+          input_fname_pattern=FLAGS.input_fname_pattern,
+          is_crop=FLAGS.is_crop,
+          checkpoint_dir=FLAGS.checkpoint_dir,
+          sample_dir=FLAGS.sample_dir)
 
-      if FLAGS.is_train:
-        dcgan.train(FLAGS)
-      else:
-        if not dcgan.load(FLAGS.checkpoint_dir):
-          raise Exception("[!] Train a model first, then run test mode")
+    if FLAGS.is_train:
+      dcgan.train(FLAGS)
+    else:
+      if not dcgan.load(FLAGS.checkpoint_dir):
+        raise Exception("[!] Train a model first, then run test mode")
 
 
-      # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
-      #                 [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
-      #                 [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
-      #                 [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
-      #                 [dcgan.h4_w, dcgan.h4_b, None])
+    # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
+    #                 [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
+    #                 [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
+    #                 [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
+    #                 [dcgan.h4_w, dcgan.h4_b, None])
 
-      # Below is codes for visualization
-      OPTION = 1
-      visualize(sess, dcgan, FLAGS, OPTION)
-  except Exception as e:
-    traceback.print_exc(file=open('/script/dcgan-tensorflow108-buggy.txt','w+'))
+    # Below is codes for visualization
+    OPTION = 1
+    visualize(sess, dcgan, FLAGS, OPTION)
+  
 
 if __name__ == '__main__':
-  try:
-    tf.app.run()
-  except Exception as e:
-    traceback.print_exc(file=open('/script/dcgan-tensorflow108-buggy.txt','w+'))
+  tf.app.run()
+  
   
